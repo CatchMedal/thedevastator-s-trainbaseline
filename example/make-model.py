1b5d835b6bbe658d559e435f485f9d64e7e53fd7
Myeonggyu Lee <lee95292@naver.com>
2022-08-20 19:14:34 +0900

    default epoch 4/36

--- example/make-model.py
+++ example/make-model.py
@@ -1,7 +1,14 @@
 import torch
 import ssl
+from efficientnet_pytorch import EfficientNet
+# from torchsummaryX import summary
+from torchinfo import summary
 ssl._create_default_https_context = ssl._create_unverified_context
-m = torch.hub.load('facebookresearch/semi-supervised-ImageNet1K-models', 'resnext50_32x4d_ssl')
-print(m)
 
-efficientnet = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_efficientnet_b0', pretrained=True)
+# resnext50 = torch.hub.load('facebookresearch/semi-supervised-ImageNet1K-models', 'resnext50_32x4d_ssl')
+# ef_b4 = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_efficientnet_b4', pretrained=True)
+ef_b7 = EfficientNet.from_pretrained('efficientnet-b7')
+
+# summary(resnext50,input_size=(64,3,256,256))
+# summary(ef_b4,input_size=(64,3,256,256))
+summary(ef_b7,input_size=(64,3,256,256))
